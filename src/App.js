b64ee988c47e9e24c38150903992fea68bbea90f
wiktor-nowak <nowak.wiktor95@gmail.com>
2023-02-22 20:20:35 +0100

    Basic setup for an app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      How many team members can I invite?
+      You can invite up to 2 additional users on the Free plan. There is no limit on 
+      team members for the Premium plan.
+
+      What is the maximum file upload size?
+      No more than 2GB. All files in your account must fit your allotted storage space.
+
+      How do I reset my password?
+      Click “Forgot password” from the login page or “Change password” from your profile page.
+      A reset link will be emailed to you.
+
+      Can I cancel my subscription?
+      Yes! Send us a message and we’ll process your request no questions asked.
+
+      Do you provide additional support?
+      Chat and email support is available 24/7. Phone lines are open during normal business hours.
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/wiktor-nowak">Wiktor Nowak</a>.
+      </div>
     </div>
   );
 }
